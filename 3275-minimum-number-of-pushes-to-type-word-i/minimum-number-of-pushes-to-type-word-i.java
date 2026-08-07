b4c19e1661790486391b2d4f863d5fd42a0b216f
shashank sharma <poonamrammurti82@gmail.com>
2026-08-07 09:16:30 +0530

    Time: 2 ms (11.72%) | Memory: 43.3 MB (40.54%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int assign_key = 2;
+
+        for (char ch : word.toCharArray()) {
+            if (assign_key > 9) {
+                assign_key = 2;
+            }
+            map.put(assign_key, map.getOrDefault(assign_key, 0) + 1);
+            result += map.get(assign_key);
+            assign_key++;
+        }
+
+        return result;
+    }
+}
