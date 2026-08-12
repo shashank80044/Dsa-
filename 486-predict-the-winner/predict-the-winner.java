13a17796baf53fec71e23edd33131fe58fd360bb
shashank sharma <poonamrammurti82@gmail.com>
2026-08-12 09:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (93.25%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,29 +1,64 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
 
-    public int solve(int i, int j, int[] nums) {
+    int[][] t;
+
+    int solve(int i, int j, int[] nums) {
+
         if (i > j) {
             return 0;
         }
+
+     
         if (i == j) {
             return nums[i];
         }
 
-        int take_i = nums[i] + Math.min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
-        int take_j = nums[j] + Math.min(solve(i, j - 2, nums), solve(i + 1, j - 1, nums));
+        
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
 
-        return Math.max(take_i, take_j);
+        int take_i = nums[i] + Math.min(
+            solve(i + 2, j, nums),
+            solve(i + 1, j - 1, nums)
+        );
+
+        
+        int take_j = nums[j] + Math.min(
+            solve(i, j - 2, nums),
+            solve(i + 1, j - 1, nums)
+        );
+
+       
+        return t[i][j] = Math.max(take_i, take_j);
     }
 
     public boolean predictTheWinner(int[] nums) {
+
         int n = nums.length;
 
-        int totalScore = Arrays.stream(nums).sum();
+       
+        t = new int[n][n];
 
-        int player1Score = solve(0, n - 1, nums);
-        int player2Score = totalScore - player1Score;
+  
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(t[i], -1);
+        }
 
-        return player1Score >= player2Score;
+     
+        int total_score = 0;
+
+        for (int num : nums) {
+            total_score += num;
+        }
+
+        int player1_score = solve(0, n - 1, nums);
+
+        
+        int player2_score = total_score - player1_score;
+
+        return player1_score >= player2_score;
     }
 }
