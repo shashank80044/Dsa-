bd42fc49be8ab60d788351c5ab33e1294f0bf6cf
shashank sharma <poonamrammurti82@gmail.com>
2026-08-19 23:27:40 +0530

    Time: 14 ms (73.48%) | Memory: 42.5 MB (34.73%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,12 +1,21 @@
 class Solution {
+
     public boolean winnerSquareGame(int n) {
+        return solveForAlice(n);
+    }
+
+    public boolean solveForAlice(int n) {
+
         boolean[] dp = new boolean[n + 1];
 
+        
+
         for (int i = 1; i <= n; i++) {
+
             for (int k = 1; k * k <= i; k++) {
-                if (!dp[i - k * k]) {
+                if (dp[i - k * k] == false) {
                     dp[i] = true;
-                    break; 
+                    break;
                 }
             }
         }
